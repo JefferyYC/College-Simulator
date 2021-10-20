0f1edf33bd7a99d54d90c5eab14b3096e92e6c1f
JefferyYC <jefferyyc@gmail.com>
2021-10-20 16:09:31 -0700

    add background, comments, var name change

---
+++ Assets/Scripts/TaskCardButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script defines behaviours for clicking a task card in the task panel
+public class TaskCardButton : MonoBehaviour
+{
+  public GameObject TasksPanel;
+
+  public GameObject Taskcard;
+
+  public void SelectTask()
+  {
+    GameObject schedulePanel = GameObject.Find("SchedulePanel");
+    ScheduleLayout schedulelayout = schedulePanel.GetComponent<ScheduleLayout>();
+    if (schedulelayout.taskcount <= 7)
+    {
+      if (schedulelayout.taskcount == 7)
+      {
+        schedulelayout.taskcount = 1;
+      }
+      GameObject oldtask = schedulelayout.findEmpty(schedulelayout.taskcount);
+      GameObject newtask = GameObject.Instantiate(Taskcard) as GameObject;
+
+      newtask.transform.parent = oldtask.transform.parent;
+      newtask.transform.rotation = oldtask.transform.rotation;
+      newtask.transform.position = oldtask.transform.position;
+      newtask.transform.localScale = oldtask.transform.localScale;
+      newtask.transform.name = "NewTask" + schedulelayout.taskcount;
+      schedulelayout.taskcount ++;
+    }
+  }
+}
